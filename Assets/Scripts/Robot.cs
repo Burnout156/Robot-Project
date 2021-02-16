2b9d5ec542add0aec4e15beb24b1a7c7ff791f02
burnout156 <brunoferreira378@gmail.com>
2021-02-16 17:58:18 -0300

    personagem andando para frente conforme a camera aponta, mas ele voa

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    public CharacterController characterController;
+    public Vector3 mouseVector;
+    public float x, z;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        mouseVector.y += Input.GetAxis("Mouse X");
+        mouseVector.z += Input.GetAxis("Mouse Y");
+        x = Input.GetAxis("Vertical");
+        z = -Input.GetAxis("Horizontal");
+
+        RotateVision();
+
+        MoveWithVision();
+    }
+
+    public void RotateVision()
+    {
+        transform.eulerAngles = new Vector3(0, mouseVector.y, mouseVector.z) * 10;
+    }
+
+    public void MoveWithVision()
+    {   
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        characterController.Move(move * Time.deltaTime * 5);     
+    }
+}
